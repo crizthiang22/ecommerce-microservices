cca256569dafb44c14926ade6be496d5cb4c7652
Cristhian Guerra <criz22@hotmail.es>
2025-04-29 23:01:15 -0500

    Added Controller to User

--- userservice/src/main/java/com/cristhiang/ecommerce_microservices/userservice/userController/UserController.java
+++ userservice/src/main/java/com/cristhiang/ecommerce_microservices/userservice/userController/UserController.java
@@ -1,31 +1,51 @@
 package com.cristhiang.ecommerce_microservices.userservice.userController;
 
-import com.cristhiang.ecommerce_microservices.userservice.userModel.User;
-import com.cristhiang.ecommerce_microservices.userservice.userRepository.UserRepository;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import com.cristhiang.ecommerce_microservices.userservice.userDTO.Request.UserRequestDTO;
+import com.cristhiang.ecommerce_microservices.userservice.userDTO.Response.UserResponseDTO;
+import com.cristhiang.ecommerce_microservices.userservice.userService.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("apiv1/users")
-class UserController {
-    private final UserRepository userRepository;
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+@RequestMapping("/api/v1/user")
+@Validated
+public class UserController {
+    private final UserService userService;
+    
+    public UserController (UserService userService){
+        this.userService = userService;
     }
 
+    @GetMapping("/{id}")
+    public UserResponseDTO getUserById(@PathVariable Long id) {
+            return userService.getUserById(id);
+        }
     @GetMapping
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public List<UserResponseDTO> getAllUsers(){
+        return userService.getAllUsers();
     }
-}
+    
+    @PostMapping
+    public UserResponseDTO createUser(@RequestBody UserRequestDTO userRequestDTO) {
+        return userService.createUser(userRequestDTO);
+    }
 
+    @PutMapping("/{id}")
+    public UserResponseDTO updateUser(@PathVariable ("id") Long userId, @RequestBody UserRequestDTO userRequestDTO) {
+        return userService.updateUser(userId, userRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long userId) {
+        userService.deleteUser(userId);
+    }
+
+
+}
